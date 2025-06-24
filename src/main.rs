224e5fe0a340c190fbc7e3bd5b4cecb944df6fdb
Olle Modin <ollemodin@outlook.com>
2025-06-24 11:16:44 +0200

    Begin work on database structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    
+mod parser;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    process::exit,
+};
+
+use crate::parser::return_request_struct;
+#[tokio::main]
+async fn main() {
+    let listener = match TcpListener::bind("127.0.0.1:7950") {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Can't bind to port 7950: {}", e);
+            exit(1);
+        }
+    };
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream).await;
+    }
+}
+async fn handle_connection(mut stream: TcpStream) {
+    let unparsed: Vec<_> = BufReader::new(&stream)
+        .lines()
+        .map(|r| match r {
+            Ok(v) => v,
+            Err(_) => String::from(""),
+        })
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let request = return_request_struct(unparsed.clone()).await;
 }
---
+++ src/parser.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+pub enum Method {
+    GET,
+    PATCH,
+    POST,
+    DELETE,
+    UNDEFINED,
+}
+
+#[derive(Debug)]
+pub struct Request {
+    pub path: String,
+    pub method: Method,
+}
+use std::thread;
+pub async fn return_request_struct(unparsed: Vec<String>) -> Request {
+    let simple_request = &unparsed[0];
+    let contents: Vec<_> = simple_request.split(" ").collect();
+    let method = return_enum(contents[0]);
+    return Request {
+        path: contents[1].to_string(),
+        method: method,
+    };
+}
+fn return_enum(string: &str) -> Method {
+    if string == "GET" {
+        return Method::GET;
+    } else if string == "PATCH" {
+        return Method::PATCH;
+    } else if string == "POST" {
+        return Method::POST;
+    } else if string == "DELETE" {
+        return Method::DELETE;
+    }
+    return Method::UNDEFINED;
+}
